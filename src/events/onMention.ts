1fc14c1d6857f2592e1a312efd34bf83e0363393
Daniel Solarte <danielsolartech@hotmail.com>
2020-07-08 01:47:15 -0500

    Add onMention and onOtherLangs channel events

---
+++ src/events/onMention.ts
@@ -0,0 +1,48 @@
+import { Message } from 'discord.js';
+import axios from 'axios';
+
+const onMention = async (message: Message): Promise<void> => {
+  if (!message.guild) {
+    return;
+  }
+
+  const index: number = Math.floor(Math.random() * 50);
+
+  let quote: string = '';
+  if (index <= 25) {
+    const data = await axios.get('https://opinionated-quotes-api.gigalixirapp.com/v1/quotes?rand=t&tags=programming');
+
+    const quotes = data.data.quotes;
+    if (quotes && quotes.length) {
+      quote = quotes[0].quote;
+    }
+  } else {
+    quote = (await axios.get('http://quotes.stormconsultancy.co.uk/random.json')).data.quote;
+  }
+
+  if (quote && quote.length) {
+    const texts: any[] = (await axios.get('https://translate.googleapis.com/translate_a/single', {
+      params: {
+        client: 'gtx',
+        sl: 'auto',
+        tl: 'es',
+        dt: 't',
+        q: quote,
+      }
+    })).data[0];
+
+    if (texts.length) {
+      const str: string[] = [];
+      for (let text of texts) {
+        str.push(text[0]);
+      }
+
+      quote = str.join('');
+    }
+
+    await message.channel.send(quote);
+    return;
+  }
+};
+
+export default onMention;
---
+++ src/events/onOtherLangs.ts
@@ -0,0 +1,76 @@
+import { Message, MessageEmbed } from 'discord.js';
+import axios from 'axios';
+
+const onOtherLangs = async (message: Message): Promise<void> => {
+  const texts: any[] = (await axios.get('https://translate.googleapis.com/translate_a/single', {
+    params: {
+      client: 'gtx',
+      sl: 'auto',
+      tl: 'es',
+      dt: 't',
+      q: message.content,
+    }
+  })).data[0];
+
+  let msg: string = '';
+  if (texts.length) {
+    const str: string[] = [];
+    for (let text of texts) {
+      str.push(text[0]);
+    }
+
+    msg = str.join('');
+  }
+
+  if (!msg.length) {
+    return;
+  }
+
+  msg = msg.replace(/<@!\s/gi, '<@!').replace(/<#\s/gi, '<#').replace(/<@\s&\s/gi, '<@&');
+
+  const emojiReplace = (msg: string, lastIndex: number): string => {
+    let firstI: number = -1;
+    let secondI: number = -1;
+    let thirdI: number = -1;
+    let isAnimated: boolean = false;
+
+    if (msg.indexOf('<a:', lastIndex) >= 0) {
+      firstI = msg.indexOf('<a:', lastIndex) + 3;
+      secondI = msg.indexOf(':', firstI);
+      thirdI = msg.indexOf('>', secondI);
+      isAnimated = true;
+    } else if (msg.indexOf('<:', lastIndex) >= 0) {
+      firstI = msg.indexOf('<:', lastIndex) + 2;
+      secondI = msg.indexOf(':', firstI);
+      thirdI = msg.indexOf('>', secondI);
+    }
+
+    if (firstI >= 0 && secondI >= 0 && thirdI >= 0) {
+      const emojiName: string = msg.slice(firstI, secondI).trim();
+      const emojiID: string = msg.slice(secondI + 1, thirdI).trim();
+      const full: string = msg.slice(isAnimated ? firstI - 3 : firstI - 2, thirdI + 1);
+
+      if (full.includes(' ')) {
+        msg = msg.replace(full, `<${isAnimated ? 'a' : ''}:${emojiName}:${emojiID}>`);
+
+        if ((msg.includes('<a:') || msg.includes('<:')) && msg.includes(' ')) {
+          return emojiReplace(msg, thirdI);
+        }
+      }
+    }
+
+    return msg;
+  };
+
+  msg = emojiReplace(msg, 0);
+
+  await message.channel.send(
+    new MessageEmbed()
+      .setDescription(msg)
+      .setColor('#009688')
+      .setFooter(message.author.username)
+      .setTimestamp(Date.now())
+  );
+};
+
+export default onOtherLangs;
