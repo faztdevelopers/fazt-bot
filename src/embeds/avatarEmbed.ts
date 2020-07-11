0f3757ba0f3a745c39287277464787e5230497bc
tati <mazzasantino1206@gmail.com>
2020-07-11 01:17:09 -0300

    fix(linting): ESlint errors

--- src/embeds/avatarEmbed.ts
+++ src/embeds/avatarEmbed.ts
@@ -3,11 +3,11 @@
 import { MessageEmbed } from 'discord.js';
 
 export default class extends MessageEmbed {
-    constructor(author: string, avatar: string) {
-      super();
-      this.color = 0xFF0022;
-      this.title = `Hey ${author}, este es tu avatar`;
-      this.image = {url: avatar};
-    }
+  constructor(author: string, avatar: string) {
+    super();
+    this.color = 0xFF0022;
+    this.title = `Hey ${author}, este es tu avatar`;
+    this.image = {url: avatar};
+  }
 
 }
